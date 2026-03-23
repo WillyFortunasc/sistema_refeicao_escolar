7f9e108804dc877fc94e26f39f6c0ef855b25a4d
LucianaCunha <luciana91cb@outlook.com>
2026-03-23 22:14:51 +0000

    ok

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.utils import timezone
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def health_check(request):
+    return Response({
+        'status': 'ok',
+        'message': 'Cantina Escolar API esta funcionando!',
+        'timestamp': timezone.now().isoformat(),
+        'version': '1.0.0'
+    })

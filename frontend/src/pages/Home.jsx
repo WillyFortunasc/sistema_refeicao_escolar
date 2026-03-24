74a55502d84bbde6c8009647c81205db4e91cce9
Willy Fortuna <willyfortunasc.unb@gmail.com>
2026-03-24 14:43:22 -0300

    atualização_frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-10 rounded-2xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Sistema de Refeição Escolar
+        </h1>
+        <p className="mt-4 text-gray-600">
+          Frontend funcionando com Tailwind 🚀
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home

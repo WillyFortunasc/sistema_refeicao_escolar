c0452bbb8ea9fcc1600bc899e77118a526b0bc63
Willy Fortuna <willyfortunasc.unb@gmail.com>
2026-04-15 15:11:30 -0300

    atualização

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
-from .views import RegistroView, LoginView, health_check, EstudanteViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    RegistroView,
+    LoginView,
+    health_check,
+    EstudanteViewSet
+)
+
+router = DefaultRouter()
+router.register(r'estudantes', EstudanteViewSet)
 
 urlpatterns = [
     path('health/', health_check),
     path('login/', LoginView.as_view()),
     path('registro/', RegistroView.as_view()),
+    path('', include(router.urls)),
 ]
